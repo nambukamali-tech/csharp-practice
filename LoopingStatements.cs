e05eae0b84064fcd707d03afc72b8be07a148d23
Nambu Kamali <nambukamali@gmail.com>
2025-10-13 22:31:13 +0530

    Add files via upload

---
+++ LoopingStatements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopingstatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of rows");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=rows;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+               
+            }
+        }
+    }
+}
