5c608a3547cb97a4cc90b509950b8d1803c8db3f
Nambu Kamali <nambukamali@gmail.com>
2025-10-24 21:26:48 +0530

    Add files via upload

---
+++ enum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENUM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bank bk = bank.current;
+            Console.WriteLine("The bank type is" + bk);
+
+        }
+        enum bank
+        {
+            current=1,
+            savings=2,
+            balance =0
+          
+        }
+    }
+}
