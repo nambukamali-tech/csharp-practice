3cf2ebe1849c928b02a4516a265c43293c553299
Nambu Kamali <nambukamali@gmail.com>
2026-02-14 23:09:03 +0530

    Add files via upload

---
+++ TodoList.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoList_API.Models
+{
+    public class TodoList
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; } = string.Empty;
+        public string TaskStatus { get; set; } = string.Empty;
+
+    }
+}
