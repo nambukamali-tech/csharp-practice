9231a49e3b5e004adabd67cd63b9684ce77a0a99
Nambu Kamali <nambukamali@gmail.com>
2026-02-14 23:08:40 +0530

    Add files via upload

---
+++ TodoListController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoList_API.Services;
+using TodoList_API.Models;
+
+namespace TodoList_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListController : ControllerBase
+    {
+        private readonly TodoServices _Service;
+        public TodoListController(TodoServices services)
+        {
+            _Service = services;
+        }
+        [HttpGet]
+        public IActionResult GetAllTasks()
+        {
+            var tasks = _Service.GetAllTasks();
+            return Ok(tasks);
+        }
+        [HttpPost]
+        public IActionResult CreateTask(TodoList task)
+        {
+            var createdTask = _Service.CreateTask(task);
+            return CreatedAtAction(nameof(GetAllTasks),
+                new { TaskId = createdTask.TaskId },
+              createdTask);
+
+
+        }
+    }
+}
