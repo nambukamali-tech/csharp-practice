9183ab822e243d0e6417044ee84d8b008043d219
Nambu Kamali <nambukamali@gmail.com>
2026-02-14 23:09:33 +0530

    Add files via upload

---
+++ TodoServices.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TodoList_API.Models;
+namespace TodoList_API.Services
+{
+    public class TodoServices
+    {
+        private readonly List<TodoList> _tasks = new List<TodoList>();
+        private int _nextTaskid = 1;
+
+        //Get all tasks
+        public List<TodoList> GetAllTasks() => _tasks;
+
+        //Create task
+        public TodoList CreateTask(TodoList task)
+        {
+            task.TaskId = _nextTaskid++;
+            _tasks.Add(task);
+            return task;
+        }
+    }
+}
